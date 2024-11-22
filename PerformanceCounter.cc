dbba6d325d35e6cf098b9bf840fcb18ba35279fa
Nicholas Mounts <nicomounts@gmail.com>
2024-11-21 22:17:12 -0500

    fixed pulling error

--- PerformanceCounter.cc
+++ PerformanceCounter.cc
@@ -1,23 +1,37 @@
-#include "PerformanceCounter.h"
+#ifndef PERFORMANCE_COUNTER_H
+#define PERFORMANCE_COUNTER_H
 
-PerformanceCounter::PerformanceCounter() : accesses(0), hits(0), misses(0) {}
+#include <iostream>
+#include <iomanip>
 
-void PerformanceCounter::incrementAccess() {
-    accesses++;
-}
+class PerformanceCounter {
+private:
+    int accesses;
+    int hits;
+    int misses;
+    int writebacks;
 
-void PerformanceCounter::incrementHit() {
-    hits++;
-}
+public:
+    PerformanceCounter() : accesses(0), hits(0), misses(0), writebacks(0) {}
 
-void PerformanceCounter::incrementMiss() {
-    misses++;
-}
+    void incrementAccess() { ++accesses; }
+    void incrementHit() { ++hits; }
+    void incrementMiss() { ++misses; }
+    void incrementWriteback() { ++writebacks; }
 
+    void displayStatistics() const {
+        // Reset manipulators
+        std::cout << std::dec << std::setfill(' ') << std::fixed << std::setprecision(2);
 
-void PerformanceCounter::displayStatistics() const {
-    std::cout << "Accesses: " << accesses << "\n";
-    std::cout << "Hits: " << hits << "\n";
-    std::cout << "Misses: " << misses << "\n";
-    std::cout << "Miss Perc: " << (accesses ? (100.0 * misses / accesses) : 0.0) << "\n";
-}
+        std::cout << std::left << std::setw(15) << "Accesses:" << std::right << accesses << "\n";
+        std::cout << std::left << std::setw(15) << "Hits:" << std::right << hits << "\n";
+        std::cout << std::left << std::setw(15) << "Misses:" << std::right << misses << "\n";
+        std::cout << std::left << std::setw(15) << "Writebacks:" << std::right << writebacks << "\n";
+        std::cout << std::left << std::setw(15) << "Miss Perc:" << std::right
+                  << (accesses ? (100.0 * misses / accesses) : 0.0) << "\n";
+        std::cout << std::left << std::setw(15) << "Writeback Perc:" << std::right
+                  << (accesses ? (100.0 * writebacks / accesses) : 0.0) << "\n";
+    }
+};
+
+#endif // PERFORMANCE_COUNTER_H
